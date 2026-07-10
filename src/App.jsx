2229a564191bf6e9d6ed6d6da3118799a094ac88
Brunda <brundaaishu@gmail.com>
2026-07-10 23:06:13 +0530

    clean up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Programs from './components/Programs'
-import Videos from './components/north star/Videos'
-import Gallery from './components/north star/Gallery'
 import Blogs from './components/Blogs'
 import Review from './components/Review'
 import Footer from './components/Footer'
 import Team  from './components/Team'
-import Experience from './pages/Experience'
 import LearnMore from "./components/Learnmore";
 
 const App = () => {
